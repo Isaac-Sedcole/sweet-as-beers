474dd5a9c2a76161fe532931625736521cfcbc0e
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2021-02-09 10:22:27 +1300

    beers adding to cart

--- client/components/BeerListItem.jsx
+++ client/components/BeerListItem.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { addToCart } from '../actions'
+import {connect} from 'react-redux'
+
 
 function BeerListItem (props) {
-  const { name, brewery, country, abv } = props.beer
+
+  const { name, brewery, country, abv, id } = props.beer
+
+  // console.log(id)
+
+  const handleClick = () => {
+    props.dispatch(addToCart(id, name))
+  }
+
   return (
     <div className='beer'>
       <p className='name'>{name}</p>
@@ -9,10 +20,10 @@ function BeerListItem (props) {
       <p>
         <span className='country'>{country}</span>
         <span className='abv'>{abv} abv</span>
-        <a href='#' className='cart-link'>Add to cart</a>
+        <a onClick={() => handleClick()} href='#' className='cart-link'>Add to cart</a>
       </p>
     </div>
   )
 }
 
-export default BeerListItem
+export default connect()(BeerListItem)
