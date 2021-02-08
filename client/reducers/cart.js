396b45190694269892fcf6b7728b71b97766b9bd
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2021-02-09 11:29:02 +1300

    quantity increments correctly

--- client/reducers/cart.js
+++ client/reducers/cart.js
@@ -5,11 +5,47 @@ const initialState = []
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART:
-      return [...state, {
-        id: action.id,
-        name: action.name,
-        quantity: 1
-      }]
+      
+      // state = state.map(items => {
+      //   if(items.id == action.id) {
+      //     items.quantity++
+      //   }else {
+      //      return [...state, {
+      //       id: action.id,
+      //       name: action.name,
+      //       quantity: 1
+      //     }]
+      //   }
+      //   return items
+      // }) 
+      // console.log(state)
+      // return state
+
+      let alreadyExists = false
+      let s = state.map(item => {
+        console.log(item)
+        if(item.id == action.id) {
+          alreadyExists = true
+          item.quantity++
+        }
+        return item
+      })
+
+      if(!alreadyExists) {
+        return [...state, {
+          id: action.id,
+          name: action.name,
+          quantity: 1
+        }]
+      } else {
+        return s 
+      }
+
+
+
+      // console.log(s)
+
+      // return s
     default :
       return state
   }
